89a763f713dd0db2211ffd9090dcd57c9799deba
Lezarius <remy.mazabraud@gmail.com>
2017-02-21 12:04:41 +0100

    copied french cv.

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Perdu(e) ?</title>
+	<link rel="stylesheet" type="text/css" href="/BootStrap/css/bootstrap.css"> 
+	<link rel="stylesheet" type="text/css" href="/BootStrap/css/footer.css">
+</head>
+<body>
+	<div>
+	<?php include 'menu.php'; ?>
+<div class="container">
+<div class="jumbotron">
+	<h1>404 Page non trouvée</h1>
+                <p>Une erreur s'est produite lorsque le petit chinois vous à apporté la page !</p>
+                <p><a class="btn btn-primary btn-lg">Retrouner as l'accueille</a></p>
+
+
+</div>
+</div>
+	<?php include 'footer.php'; ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,332 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+  <meta name="description" content="CV de Rémy Mazabraud" />
+  <link rel="icon" type="image/png" href="favicon.ico" />
+<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /><![endif]-->
+<meta name="viewport" content="initial-scale=1"/>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="BootStrap/css/bootstrap.css"> 
+	<link rel="stylesheet" type="text/css" href="BootStrap/zocial/zocial.css">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+</head>
+<body>
+	<?php include 'menu.php'; ?>
+
+<div class="container">
+	
+
+
+<!-- Row de présentation -->
+<div class="row">
+ <!-- Grande colonne  -->
+  <div class="col-md-9 col-md-push-3">
+  <h1 align="center"><span class="glyphicon glyphicon-globe"></span></h1>
+<h1 align="center"> Une petite présentation s'impose ! </h1>
+<article>
+Je m'appelle Ronce Akaé Lezarius. Je suis une scientifique du web, je touche à tout et essaye de réaliser ma vision du web et de la vie de demain.
+Passionnée par les nouvelles technologies et la politique, je m’intéresse aussi au retro gaming. Je réalise aussi quelques billets engagés
+sur le monde actuel et mes geekerie.
+<br> 
+<br> 
+Pour autant, je reste ouvert d'esprit sur le monde qui m'entoure et c'est cela qui fait ma force !
+</article>
+
+<h1>Une autre façon de me voir</h1>
+<h3>Lezarius est mon pseudo sur les internets !</h3>
+<h6>Vous pouvez donc me retrouver sur différentes platformes :</h6>
+<p><a href="http://www.twitter.com/lezarius" class="zocial twitter">@Lezarius</a>
+	<a href="https://github.com/lezarius" class="zocial github">@Lezarius</a>
+  </div>
+  <!-- Petite colonne  -->
+  <div class="col-md-3 col-md-pull-9">
+  	<h3><span class="glyphicon glyphicon-user"></span>Ronce Mazabraud</h3>
+<!-- <img src="img/profils.png" alt="" height="250" width="200">
+<h4>Né le : </h4>
+<p>30 Janvier 1996</p> 
+<h4>Vit à l'adresse :</h4>
+<p>Résidence GasVotre Cabannes Batiment 11 Appartement 115 </br>
+31 Avenue GasVotre Cabannes </br>
+33270 Floirac
+</p>-->
+  </div>
+</div>
+<!-- Mes expériences -->
+<div class="row" id="xp">
+<h1 align="center">Mes expériences</h1>
+<h1 align="center"><span class="glyphicon glyphicon-briefcase"></span>	</h1>
+<div class="row">
+<h3>En 2013 :</h3>
+          <div class="col-lg-6">
+            <blockquote>
+              <p>Création d'une identité visuelle et communication d'une société.</p>
+              <small>Heuzef 33000,Bordeaux</small>
+            </blockquote>
+          </div>
+          <div class="col-lg-6">
+            <blockquote class="pull-right">
+              <p>Aide à la conception et analyse des techniques d'architecture moderne.</p>
+              <small>Cokaliti Illustration 33270, Floirac</small>
+            </blockquote>
+          </div>
+<div class="col-lg-6">
+            <blockquote>
+              <p>Mise en situation en tribunal et étude d'une affaire criminelle.</p>
+              <small>Maître Robert 33000, Bordeaux</small>
+            </blockquote>
+          </div>
+</div>
+<div class="row">
+<h3>En 2012 :</h3>
+          <div class="col-lg-6">
+            <blockquote>
+              <p>Intégration de contenus pour différents sites web.</p>
+              <small>Incomm 33270, Floirac</small>
+            </blockquote>
+          </div>
+
+</div>
+<div class="row">
+<h3>En 2011 :</h3>
+          <div class="col-lg-6">
+            <blockquote>
+              <p>Vente et relation client dans un centre commercial.</p>
+              <small>Mag Presse 33270, Floirac</small>
+            </blockquote>
+          </div>
+
+</div>
+<div class="row">
+<h3>En 2010 :</h3>
+          <div class="col-lg-6">
+            <blockquote>
+              <p>Apprentissage des techniques de réalitation de bijoux et de tracés, conception d'une alliance et de différentes pièces en acier.</p>
+              <small>Bijouterie Bernard Espina 33000, Bordeaux</small>
+            </blockquote>
+          </div>
+
+</div>
+<div class="row" id="diplome">
+	<h1 align="center">Mes diplômes</h1>
+	<h1 align="center"><span class="glyphicon glyphicon-folder-open"></span></h1>
+          <div class="col-lg-6">
+            <blockquote>
+              <p>Diplôme National du Brevet des Collèges</p>
+              <small>Maison Familiale du Blayais 2011 à 2013</small>
+            </blockquote>
+          </div>
+          <div class="col-lg-6">
+            <blockquote class="pull-right">
+              <p>Brevet de secourisme</p>
+              <small>Maison Familiale du Blayais 2011 à 2013</small>
+            </blockquote>
+          </div>
+
+</div>
+<!-- Mes compétences -->
+<div class="row" id="competence">
+<div class="col-md-9 col-md-push-3">
+	<h1 align="center">Mes compétences</h1>
+	<h1 align="center"><span class="fa fa-gear fa-spin fa-2x "></span></h1>
+     <div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">HTML & CSS
+    <span class="sr-only">80% Complete</span>
+  </div>
+</div>
+<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">PHP
+    <span class="sr-only">60% Complete</span>
+  </div>
+</div>
+<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 65%;">linux
+    <span class="sr-only">80% Complete</span>
+  </div>
+</div>
+<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">Réseaux
+    <span class="sr-only">40% Complete</span>
+  </div>
+</div>
+<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 85%;">Git
+    <span class="sr-only">60% Complete</span>
+  </div>
+</div>
+<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 85%;">
+  Team Work gestion
+    <span class="sr-only">70% Complete</span>
+  </div>
+</div>
+</div>
+
+
+<div class="col-md-3 col-md-pull-9">
+<h1>Mes atouts</h1>
+<ul>
+	<li>Esprit d'équipe</li>
+	<li>Niveau d'adaptation élevé</li>
+	<li>Esprit d'entreprise</li>
+	<li>Veille technologique</li>
+</ul>
+</div>
+</div>
+
+<div>
+<?php
+//If the form is submitted
+if(isset($_POST['submit'])) {
+
+  //Check to make sure that the name field is not empty
+  if(trim($_POST['contactname']) == '') {
+    $hasError = true;
+  } else {
+    $name = trim($_POST['contactname']);
+  }
+
+  //Check to make sure that the phone field is not empty
+  if(trim($_POST['phone']) == '') {
+    $hasError = true;
+  } else {
+    $phone = trim($_POST['phone']);
+  }
+
+  //Check to make sure that the name field is not empty
+  if(trim($_POST['weburl']) == '') {
+    $hasError = true;
+  } else {
+    $weburl = trim($_POST['weburl']);
+  }
+
+  //Check to make sure that the subject field is not empty
+  if(trim($_POST['subject']) == '') {
+    $hasError = true;
+  } else {
+    $subject = trim($_POST['subject']);
+  }
+
+  //Check to make sure sure that a valid email address is submitted
+  if(trim($_POST['email']) == '')  {
+    $hasError = true;
+  } else if (!filter_var( trim($_POST['email'], FILTER_VALIDATE_EMAIL ))) {
+    $hasError = true;
+  } else {
+    $email = trim($_POST['email']);
+  }
+
+  //Check to make sure comments were entered
+  if(trim($_POST['message']) == '') {
+    $hasError = true;
+  } else {
+    if(function_exists('stripslashes')) {
+      $comments = stripslashes(trim($_POST['message']));
+    } else {
+      $comments = trim($_POST['message']);
+    }
+  }
+
+  //If there is no error, send the email
+  if(!isset($hasError)) {
+    $emailTo = 'remy.mazabraud@gmail.com'; // Put your own email address here
+    $body = "Name: $name \n\nEmail: $email \n\nPhone Number: $phone \n\nSubject: $subject \n\nComments:\n $comments";
+    $headers = 'From: Contact Pro CV <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+    mail($emailTo, $subject, $body, $headers);
+    $emailSent = true;
+  }
+}
+?>
+
+  <div class="row" id="contact">
+
+    <div class="row">
+      <div class="col-md-6 col-md-push-3">
+      <div class="jumbotron">
+        <form role="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactform">
+          <fieldset>
+            <legend>Me contacter :</legend>
+
+            <?php if(isset($hasError)) { //If errors are found ?>
+              <p class="alert alert-danger">Verifiez les champs que vous avez remplis. Merci.</p>
+            <?php } ?>
+
+            <?php if(isset($emailSent) && $emailSent == true) { //If email is sent ?>
+              <div class="alert alert-success">
+                <p><strong>Message envoyé !</strong></p>
+                <p>Merci pour le petit mot, <strong><?php echo $name;?></strong>! Votre message m'a bien été transmis !</p>
+              </div>
+            <?php } ?>
+
+            <div class="form-group">
+              <label for="name">Votre Nom/Prénom<span class="help-required">*</span></label>
+              <input title="ContactName" type="text" name="contactname" id="contactname" value="" class="form-control required" role="input" aria-required="true" />
+            </div>
+
+            <div class="form-group">
+              <label for="phone">Votre numéro de téléphone<span class="help-required">*</span></label>
+              <input type="text" name="phone" id="phone" value="" class="form-control required" role="input" aria-required="true" />
+            </div>
+
+
+            <div class="form-group">
+              <label for="email">Votre Email<span class="help-required">*</span></label>
+              <input type="text" name="email" id="email" value="" class="form-control required email" role="input" aria-required="true" />
+            </div>
+
+            <div class="form-group">
+              <label for="weburl">Votre Site Web<span class="help-required">*</span></label>
+              <input type="text" name="weburl" id="weburl" value="" class="form-control required url" role="input" aria-required="true" />
+            </div>
+
+
+            <div class="form-group">
+              <label for="subject">Sujet<span class="help-required">*</span></label>
+              <select name="subject" id="subject" class="form-control required" role="select" aria-required="true">
+                <option></option>
+                <option>Me contacter</option>
+                <option>Un projet ?</option>
+                <option>Un contrat ?</option>
+                <option>Autre?</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <label for="message">Message<span class="help-required">*</span></label>
+              <textarea rows="8" name="message" id="message" class="form-control required" role="textbox" aria-required="true"></textarea>
+            </div>
+
+            <div class="actions">
+              <input type="submit" value="Envoyer le message" name="submit" id="submitButVotre" class="btn btn-primary" title="Click here to submit your message!" />
+              <input type="reset" value="Netoyer le formulaire" class="btn btn-danger" title="Remove all the data from the form." />
+            </div>
+          </fieldset>
+        </form>
+        </div>
+      </div><!-- col -->
+    
+
+<div class="col-md-3 col-md-pull-6">
+<h1>Autre site que j'ai produit: </h1>
+<ul>
+	<li>Sur mon <a href="http://blog.lezarius.science">blog</a></li>
+	<li>Sur mon site <a href="http://lezarius.science">Perso</a></li>
+	<li>Sur mon site <a href="http://lezarius.tech">Pro</a></li>
+	
+</ul>
+</div>
+
+
+    </div><!-- row -->
+
+</div>
+
+
+
+<!-- DIV FINAL -->
+</div>
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
